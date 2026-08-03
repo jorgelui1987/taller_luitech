08a01c2336d493fba50d31536125b8675d6fed91
jorgelui1987 <jorgelui1987@users.noreply.github.com>
2026-08-02 20:38:27 -0400

    Actualizar vista de creación de clientes

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -97,7 +97,7 @@
                         <div class="col-md-4">
                             <label class="form-label">Ciudad</label>
                             <input type="text" class="form-control" name="ciudad"
-                                   value="{{ old('ciudad') }}" placeholder="Lima">
+                                   value="{{ old('ciudad') }}" placeholder="">
                         </div>
                         <div class="col-12">
                             <label class="form-label">Dirección</label>
