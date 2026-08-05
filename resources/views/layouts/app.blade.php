1f7ec265b3529b414395e5863960112d1fa1c93e
jorgelui1987 <jorgelui1987@users.noreply.github.com>
2026-08-04 23:16:58 -0400

    Fix: quitar integrity hash que bloqueaba Bootstrap JS y los modales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1077,7 +1077,7 @@
 @endif
 
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-0p+9gR2E5qXe7RNrG7hR5lPaYST7MLKq7b/MQ8/hRqE9GPHev3eA6Q6J0xVLQeQ" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Chart.js -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script><!-- NOSONAR -->
 
